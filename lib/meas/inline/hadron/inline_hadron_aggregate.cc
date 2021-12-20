4337e939fef3d971d4f1800f91b1ab07c74af556
Travis H. Whyte <thwhyte@femto.sciclone.wm.edu>
2021-12-20 13:52:44 -0500

    adding frequency splitting and interpolation

--- lib/meas/inline/hadron/inline_hadron_aggregate.cc
+++ lib/meas/inline/hadron/inline_hadron_aggregate.cc
@@ -20,6 +20,7 @@
 #include "meas/inline/hadron/inline_disco_w.h"
 #include "meas/inline/hadron/inline_disco_prob_defl_w.h"
 #include "meas/inline/hadron/inline_disco_prob_defl_superb_w.h"
+#include "meas/inline/hadron/inline_disco_frequencysplitting_prob_superb_w.h"
 #include "meas/inline/hadron/inline_disco_eoprec_w.h"
 #include "meas/inline/hadron/inline_disco_eo_eigcg_w.h"
 #include "meas/inline/hadron/inline_disco_eigcg_w.h"
@@ -142,6 +143,7 @@ namespace Chroma
 	success &= InlineDiscoEigCGEnv::registerAll();
 	success &= InlineDiscoProbDefl::registerAll();
 	success &= InlineDiscoProbDeflSuperb::registerAll();
+	success &= InlineDiscoFreqSplitProbSuperbV2::registerAll();
 
 	success &= InlineStagToWilsEnv::registerAll();
 	success &= InlineSinkSmearEnv::registerAll();
