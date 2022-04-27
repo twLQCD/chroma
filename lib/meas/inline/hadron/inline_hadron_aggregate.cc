4b2aa1b9bc7aabd674a2681e1d0e6842fdeb9179
Travis H. Whyte <thwhyte@femto.sciclone.wm.edu>
2022-04-27 09:40:06 -0400

    Added RFS

--- lib/meas/inline/hadron/inline_hadron_aggregate.cc
+++ lib/meas/inline/hadron/inline_hadron_aggregate.cc
@@ -21,6 +21,7 @@
 #include "meas/inline/hadron/inline_disco_prob_defl_w.h"
 #include "meas/inline/hadron/inline_disco_prob_defl_superb_w.h"
 #include "meas/inline/hadron/inline_disco_frequencysplitting_prob_multimass_superb_w.h"
+#include "meas/inline/hadron/inline_disco_recursive_fs_prob_multimass_superb_w.h"
 #include "meas/inline/hadron/inline_disco_frequencysplitting_multimass_superb_w.h"
 #include "meas/inline/hadron/inline_disco_eoprec_w.h"
 #include "meas/inline/hadron/inline_disco_eo_eigcg_w.h"
@@ -151,6 +152,7 @@ namespace Chroma
 #ifdef BUILD_SB
 	success &= InlineDiscoProbDeflSuperb::registerAll();
 	success &= InlineDiscoFreqSplitProbMMSuperb::registerAll();
+	success &= InlineDiscoRecFreqSplitProbMMSuperb::registerAll();
 	success &= InlineDiscoFreqSplitMMSuperb::registerAll();
 #endif
 
